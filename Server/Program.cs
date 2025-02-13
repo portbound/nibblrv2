6340627f4e2642ed3d056fa466e757e2087d9a62
Jake Levy <77429039+sushi-tears@users.noreply.github.com>
2025-02-12 20:54:28 -0500

    adding initial files

---
+++ Server/Program.cs
@@ -0,0 +1,56 @@
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+using Server.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddOpenApi();
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment()) {
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+    app.UseWebAssemblyDebugging();
+}
+
+app.UseHttpsRedirection();
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+app.UseRouting();
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+
+app. Run();
+// var summaries = new[] {
+//     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+// };
+//
+// app.MapGet("/weatherforecast", () =>
+//     {
+//         var forecast = Enumerable.Range(1, 5).Select(index =>
+//                 new WeatherForecast
+//                 (
+//                     DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+//                     Random.Shared.Next(-20, 55),
+//                     summaries[Random.Shared.Next(summaries.Length)]
+//                 ))
+//             .ToArray();
+//         return forecast;
+//     })
+//     .WithName("GetWeatherForecast");
+//
+// app.Run();
+//
+// record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary) {
+//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+// }
